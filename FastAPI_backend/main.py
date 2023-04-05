b409ff38544528b7e4662a4c1943aa9986c552f4
Dang Minh Duc <ducwall@gmail.com>
2023-04-06 01:07:23 +0700

    Dang Minh Duc - Python backend

---
+++ FastAPI_backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PRIVATE_KEY = "dc344cfe-6bb1-427c-a8e0-a7481da7136d"
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Project-ID": PRIVATE_KEY }
+    )
+    return response.json()
